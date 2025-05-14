9554f8fdc5b1751ee31aed3325d42574b06a74ea
PribludaVolka <stepanpan8@gmail.com>
2025-05-14 16:45:42 +0300

    step 2?

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { StudentModule } from './student/student.module';
+import { UserModule } from './user/user.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { ParentModule } from './parent/parent.module';
+import { LessonModule } from './lesson/lesson.module';
+import { GradeModule } from './grade/grade.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { ClassModule } from './class/class.module';
+import { SubjectModule } from './subject/subject.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, StudentModule, UserModule, TeacherModule, ParentModule, LessonModule, GradeModule, ScheduleModule, ClassModule, SubjectModule],
 })
 export class AppModule {}
